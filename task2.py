85328983eedd1bdefb7c9fdd26129966dec389f0
Nandini soni <nandinisoni4321@gmail.com>
2026-06-11 11:19:52 +0530

    Updated Task 2

--- task2.py
+++ task2.py
@@ -1,4 +1,24 @@
 #File Handling 
+#using with function
+
+# with open("notes.txt", "r") as f:
+#     print(f.read())
+    
 
 
+# with open("notes.txt",  "w") as f:
+#     f.write("maincrafts technology")
+#     print("overwite the file")
 
+    
+
+# with open("notes.txt",  "a") as f :
+#     f.write("\npython programing domain")
+
+
+#CSV Basics
+
+import csv #import csv module
+with open("expenses.csv", "a", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["lunch", 120])
