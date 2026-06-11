8228d6f3b9b41486a9c2ad0768379eade1aa0eef
Nandini soni <nandinisoni4321@gmail.com>
2026-06-11 11:39:38 +0530

    Updated Task 2

--- task2.py
+++ task2.py
@@ -18,7 +18,26 @@
 
 #CSV Basics
 
-import csv #import csv module
-with open("expenses.csv", "a", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["lunch", 120])
+# import csv #import csv module
+# with open("expenses.csv", "a", newline="") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["lunch", 120])
+
+#mini project making function to save expenses in csv file
+
+import csv
+
+def add_expense(desc, amount):
+
+    with open("expenses.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([desc, amount])
+        
+
+#calling function
+
+add_expense("Lunch", 120)
+add_expense("Movie", 250)
+
+
+
