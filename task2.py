ed12115c4d3b63bbb091e04f96eafd34592f741b
Nandini soni <nandinisoni4321@gmail.com>
2026-06-13 10:17:12 +0530

    updated Task 2 Python programs

--- task2.py
+++ task2.py
@@ -25,19 +25,94 @@
 
 #mini project making function to save expenses in csv file
 
-import csv
+# import csv
 
-def add_expense(desc, amount):
+# def add_expense(desc, amount):
 
-    with open("expenses.csv", "a", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow([desc, amount])
+#     with open("expenses.csv", "a", newline="") as f:
+#         writer = csv.writer(f)
+#         writer.writerow([desc, amount])
         
 
+# #calling function
+
+# add_expense("Lunch", 120)
+# add_expense("Movie", 250)
+
+#Function + menu driven program + while loop
+# def greet():
+#     print("Hello")
 #calling function
-
-add_expense("Lunch", 120)
-add_expense("Movie", 250)
+# greet()
 
 
+# while True:
 
+#     num = input("Enter x to exit: ")
+
+#     if num == "x":
+#         break
+
+# while True:
+#     print("\n expenses tracker")
+    
+#     print("1. add expenses")
+#     print("2. view expenses")
+#     print("3.exit")
+    
+#     choice = input("enter choice")
+    
+#     if choice == "1":
+#         print("add expenses selected")
+#     elif choice == "2":
+#         print("view expenses selected")
+#     elif choice == "3":
+#         print("program closed")
+#         break
+    
+        
+#     else:
+#         print("invalid choice")    
+    
+# view expenses and total spent
+
+# import csv
+
+# with open("expenses.csv", "r") as f:
+
+#     reader = csv.reader(f)
+
+#     for row in reader:
+#         print(row)
+
+# import csv
+
+# def view_expenses():
+
+#     with open("expenses.csv", "r") as f:
+
+#         reader = csv.reader(f)
+
+#         for row in reader:
+
+#             print(row[0], "-", row[1])
+
+# view_expenses()
+            
+            
+import csv
+
+def total_spent():
+
+    total = 0
+
+    with open("expenses.csv", "r") as f:
+
+        reader = csv.reader(f)
+
+        for row in reader:
+
+            total = total + int(row[1])
+
+    print("Total =", total)
+total_spent()
